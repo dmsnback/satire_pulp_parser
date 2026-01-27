1c12c7cc6abe7c7d66f20ebefa788010df957d21
Dmitry Titenkov <lt200711@yandex.ru>
2026-01-27 14:37:44 +0300

    add sqlite3

--- satire_pulp_parser/spiders/satire_pulp.py
+++ satire_pulp_parser/spiders/satire_pulp.py
@@ -1,16 +1,24 @@
 import scrapy
+from storage import init_db, is_news_exists, save_news
 
 
 class SatirePulpSpider(scrapy.Spider):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        init_db()
+
     name = "satire_pulp"
     allowed_domains = ["panorama.pub"]
     start_urls = ["https://panorama.pub"]
 
     def parse(self, response):
-        self.logger.info("Главная страница Панорамы")
         news_links = response.css("div.shrink-0 li a::attr(href)").getall()
         for link in news_links:
             full_link = response.urljoin(link)
+            if is_news_exists(full_link):
+                self.logger.info(f"Новость уже есть: {full_link}")
+                continue
+
             yield scrapy.Request(url=full_link, callback=self.parse_news)
 
     def parse_news(self, response):
@@ -18,6 +26,10 @@ class SatirePulpSpider(scrapy.Spider):
         text = response.css("div.entry-contents p::text").getall()
         image = response.css('meta[itemprop="image"]::attr(content)').get()
         final_text = " ".join(text).strip()
-        self.logger.info(f"ЗАГОЛОВОК: {title}")
-        self.logger.info(f"ТЕКСТ: {final_text}")
-        self.logger.info(f"КАРТИНКА: {image}")
+        save_news(response.url, title)
+        yield {
+            "title": title,
+            "text": final_text,
+            "image": image,
+            "url": response.url,
+        }
