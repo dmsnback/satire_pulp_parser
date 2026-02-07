f496e0b3f8950057cf685c53c2e7aa529a49d03c
Dmitry Titenkov <lt200711@yandex.ru>
2026-02-07 12:18:08 +0300

    scheduler

--- scheduler.py
+++ scheduler.py
@@ -27,20 +27,6 @@ def run_spider():
         logger.error(f"Неожиданная ошибка при запуске парсера: {e}")
 
 
-def run_bot():
-    app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
-    app.job_queue.run_repeating(auto_send_news, interval=10, first=10)
-    app.add_handler(CommandHandler("start", menu))
-    app.add_handler(CommandHandler("show_news", show_news_command))
-    app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(CallbackQueryHandler(button_handler))
-
-    asyncio.get_event_loop().run_until_complete(set_commands(app))
-    logger.info("... Бот запущен ...")
-    app.run_polling()
-    logger.info("... Бот остановлен ...")
-
-
 def run_scheduler():
     scheduler = BlockingScheduler()
     try:
